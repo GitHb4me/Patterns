20ae9cfd19c544e18a15cd0f871094ae290619ee
GitHb4me <92459398+GitHb4me@users.noreply.github.com>
2022-07-24 01:46:48 +0530

    Add files via upload
    Kite Pattern

---
+++ kite.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using std::cout;
+using std::cin;
+ int checkspace(int h)
+{ 
+   for(int b=0;b<h;b++){
+    cout<<" ";}
+    return 0;
+}
+int rev(int k){
+for(int b=k;b>0;b--)
+{
+checkspace(k-b); // try this ----> checkspace(j-b+1); when for(int a=b-2;a>0;a--) 
+for(int a=b-1;a>0;a--)
+{                            
+cout<<'*'<<" ";
+}cout<<"\n";}
+return 0;
+}
+int main(){
+int n;
+cin>>n;
+int k=n-1;      
+//for(int b=1;b<=n*2-1;b++){     //n*2-1 converts any number to odd number while n*2 converts any number to even also doubles the number
+//for(int a=1;a<=b*2-1;a++)
+for(int b=0;b<n;b++)
+{
+checkspace(k-b); 
+for(int a=0;a<b;a++)
+{                            
+cout<<'*'<<" ";
+}cout<<"\n";}
+rev(n);
+}  
+/*
+1
+1 2 3
+1 2 3 4 5
+1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9  this program prints following output*/
